376b926212811d0f3ce2c7e2954b72cc58c5d6b3
Соловьев Валерий Александрович <Valery.Solovev@innostage-group.ru>
2023-03-14 18:40:02 +0300

    Homework3

---
+++ src/main/java/FinderFiles.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class FinderFiles {
+    private static int countFiles = 0;
+    private static int countFolders = 0;
+
+    public static void readFolder(String pathToFile){
+        File dir = new File(pathToFile);
+        if (dir.isFile()) {
+            countFiles += 1;
+        } else {
+            if (dir.isDirectory()){
+                countFolders += 1;
+                for (File file: Objects.requireNonNull(dir.listFiles())
+                     ) { readFolder(String.valueOf(file));
+                }
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        readFolder("src");
+        System.out.println("Количество файлов " + countFiles);
+        System.out.println("Количество папок " + countFolders);
+
+    }
+}
